3853dc7ce7847ac31e1ed83d18ce619537833c29
vladik1998 <shembel@post.bgu.ac.il>
2022-11-22 17:31:03 +0200

    To new branch, coalition rule of 5

--- src/Coalition.cpp
+++ src/Coalition.cpp
@@ -4,6 +4,44 @@ Coalition::Coalition(int id): mId(id), mTotalMandates(0) , mParties(new vector<i
 {
 }
 
+Coalition::Coalition(const Coalition& other) : mId(other.mId), mTotalMandates(other.mTotalMandates), mParties(new vector<int>(*other.mParties))
+{}
+
+Coalition::~Coalition()
+{
+    if(mParties!=nullptr){
+        delete mParties;
+    }
+}
+
+Coalition& Coalition::operator=(const Coalition& other)
+{
+    if(this != &other){
+        delete mParties;
+        mParties=new vector<int>(*other.mParties);
+        mId=other.mId;
+        mTotalMandates=other.mTotalMandates;
+    }
+    return *this;
+}
+
+Coalition::Coalition(Coalition&& other) : mId(other.mId), mTotalMandates(other.mTotalMandates), mParties(other.mParties)
+{
+    other.mParties=nullptr;
+}
+
+Coalition& Coalition::operator=(Coalition&& other)
+{
+    if(this != &other){
+        delete mParties;
+        mParties=other.mParties;
+        mId=other.mId;
+        mTotalMandates=other.mTotalMandates;
+        other.mParties=nullptr;
+    }
+    return *this;
+}
+
 void Coalition::addParty(int partyId, int partyMandates)
 {
     // assumes party not in the coalition yet
